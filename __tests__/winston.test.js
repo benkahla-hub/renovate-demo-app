d4028669c0e1540e188a7c9b86dcaec29da44a25
benkahla-hub <57709429+benkahla-hub@users.noreply.github.com>
2026-03-17 16:03:23 +0100

    Update winston.test.js

--- __tests__/winston.test.js
+++ __tests__/winston.test.js
@@ -4,5 +4,4 @@ test('erstellt einen Logger und loggt eine Nachricht', () => {
   const logger = winston.createLogger({
     transports: [new winston.transports.Console({ silent: true })],
   });
-  logger.info('Testmeldung');
 });
